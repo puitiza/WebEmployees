07992f12ab5a5aae3504456e53e3be3f35cdcef4
Anthony Puitiza Lopez <anthony.puitiza.02@gmail.com>
2019-11-01 20:56:48 -0500

    agregando componente y service necesarios en el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AddComponent } from './Persona/add/add.component';
+import { EditComponent } from './Persona/edit/edit.component';
 
-
-const routes: Routes = [];
+// En esta clase es donde definimos nuestros componentes que se usaran en el proyecto
+const routes: Routes = [
+  {path:'listar', component:ListarComponent},
+  {path:'add', component:AddComponent},
+  {path:'edit', component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebEmployees';
+
+  //Crearé un constructor de enrutamiento
+  constructor(private router:Router){}
+
+  //Recuerda que el nombre que va dentro de navigate es el mismo definido en la clase app-routing.module.ts
+  Listar(){
+    this.router.navigate(["listar"]);
+  }
+
+  Nuevo(){
+    this.router.navigate(["add"]);
+  }
 }
